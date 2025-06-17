c080705c38eda6a340c79251a20b581775fa7c56
lezhihao <lzh3606@mail.ustc.edu.cn>
2025-06-17 02:18:17 +0000

    feat: cpp overlap supported

--- fuse_kernel/fuse.cpp
+++ fuse_kernel/fuse.cpp
@@ -5,24 +5,79 @@
 #include <iostream>
 #include "ep.hpp"
 
-int main() {
+void process_args(int argc, char** argv, bool& enable_overlap, bool& enable_traverse) {
+    if (argc <= 1) {
+        std::cerr << "No parameters provided. Using default values." << std::endl;
+        return;
+    }
+    if (argc >= 2) {
+        std::string arg = argv[1];
+        if (arg == "0" || arg == "false" || arg == "False") {
+            enable_overlap = false;
+        } else if (arg == "1" || arg == "true" || arg == "True") {
+            enable_overlap = true;
+        } else {
+            std::cerr << "Usage: " << argv[0] << " [0|1|true|True|false|False]" << std::endl;
+        }
+    } 
+    if (argc >= 3) {
+        std::string arg = argv[2];
+        if (arg == "0" || arg == "false" || arg == "False") {
+            enable_traverse = false;
+        } else if (arg == "1" || arg == "true" || arg == "True") {
+            enable_traverse = true;
+        } else {
+            std::cerr << "Usage: " << argv[0] << " [0|1|true|True|false|False]" << std::endl;
+        }
+    }
+}
+
+void ep_moe_traverse(int rank, int world_size, bool enable_overlap) {
+    std::vector<uint64_t> num_tokens_l = {32, 64, 128, 256, 512};
+    std::vector<uint64_t> num_experts_l = {32, 64, 128};
+    std::vector<uint64_t> hidden_l = {4096};
+    std::vector<uint64_t> khidden_l = {3072};
+    for (auto num_tokens: num_tokens_l) {
+        for (auto num_experts: num_experts_l) {
+            for (auto hidden: hidden_l) {
+                for (auto khidden: khidden_l) {
+                    if (rank == 0) {
+                        std::cout << "num_experts: " << num_experts << ", num_tokens: " << num_tokens << ", hidden: " << hidden << ", khidden: " << khidden << std::endl;
+                    }
+                    ep_moe(num_experts, 1024/*num_max_dispatch_tokens_per_rank*/, khidden, hidden, num_tokens, 8/*num_topk*/, world_size, enable_overlap);
+                }
+            }
+        }
+    }
+}
+
+int main(int argc, char** argv) {
     const char* env_rank = std::getenv("OMPI_COMM_WORLD_RANK");
     const char* env_world_size = std::getenv("OMPI_COMM_WORLD_SIZE");
 
     int rank = env_rank ? std::atoi(env_rank) : -1;
     int world_size = env_world_size ? std::atoi(env_world_size) : -1;
 
+    bool enable_overlap = false;
+    bool enable_traverse = false;
+    process_args(argc, argv, enable_overlap, enable_traverse);
+
     init_distributed(rank, world_size);
     CUDA_CHECK(cudaSetDevice(rank));
     
-    ep_moe(
-        128/*num_experts*/, 
-        1024/*num_max_dispatch_tokens_per_rank*/, 
-        3072/*khidden*/, 
-        4096/*hidden_size*/, 
-        32/*num_tokens*/, 
-        8/*num_topk*/, 
-        world_size);
+    if (enable_traverse) {
+        ep_moe_traverse(rank, world_size, enable_overlap);
+    } else {
+        ep_moe(
+            128/*num_experts*/, 
+            1024/*num_max_dispatch_tokens_per_rank*/, 
+            3072/*khidden*/, 
+            4096/*hidden_size*/, 
+            32/*num_tokens*/, 
+            8/*num_topk*/, 
+            world_size,
+            enable_overlap);
+    }
 
     destroy_distributed();
     return 0;
