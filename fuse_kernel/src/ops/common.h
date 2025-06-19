20da8ac62a6e79fdd1dc10f557f6709291d4069a
lezhihao <lzh3606@mail.ustc.edu.cn>
2025-06-19 08:43:39 +0000

    feat: support silu and mul ops

---
+++ fuse_kernel/src/ops/common.h
@@ -0,0 +1,9 @@
+#pragma once
+
+void fuse_silu_and_mul_masked(
+    torch::Tensor& out,       // [..., d]
+    torch::Tensor& o_vec,       // [..., d]
+    torch::Tensor& o_scales,       // [..., d]
+    torch::Tensor& input,    // [..., 2*d]
+    torch::Tensor& counts,
+    int max_tokens_per_block);
