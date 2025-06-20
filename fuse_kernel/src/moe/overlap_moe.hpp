a4b0b95b58b74f90c13d48a5935fb2b68f1f89ff
lezhihao <lzh3606@mail.ustc.edu.cn>
2025-06-20 09:15:35 +0000

    [refactor] moe abstract

---
+++ fuse_kernel/src/moe/overlap_moe.hpp
@@ -0,0 +1,77 @@
+#pragma once
+
+#include "base_moe.hpp"
+
+using namespace deep_ep;
+using namespace c10d;
+
+class OverlapMoE : public BaseMoE {
+protected:
+    void _moe_core(std::shared_ptr<FUSEConfig>& fuse_config, bool enable_profile) override {
+        cudaStream_t current_stream = at::cuda::getCurrentCUDAStream();
+        CUDA_CHECK(cudaStreamCreateWithFlags(&current_stream, cudaStreamNonBlocking));
+        if (enable_profile) cudaProfilerStart();
+        global_pg->barrier()->wait();
+        torch::Tensor packed_recv_count_2 = torch::bincount(topk_ids.view(num_tokens * num_topk), {}, num_experts).to(torch::kCUDA) * world_size;
+        packed_recv_count_2 = packed_recv_count_2.index({torch::indexing::Slice(global_pg->getRank() * num_groups, (global_pg->getRank() + 1) * num_groups)});
+        cudaDeviceSynchronize();
+        
+        auto [
+            packed_recv_x, 
+            packed_recv_x_scales, 
+            packed_recv_count, 
+            packed_recv_src_info, 
+            packed_recv_layout_range, 
+            event, 
+            hook
+        ] = buffer->low_latency_dispatch(hidden_states, topk_ids, num_max_dispatch_tokens_per_rank, num_experts, fuse_config->ep_sms, true/*use_fp8*/, false/*async_finish*/, false/*return_recv_hook*/);
+        get_function_for_gemm(num_tokens, khidden, hidden_size, num_groups, fuse_config->gemm_sms)(
+            std::get<0>(x_fp8).data_ptr(), std::get<1>(x_fp8).data_ptr(),
+            std::get<0>(y_fp8).data_ptr(), std::get<1>(y_fp8).data_ptr(),
+            out.data_ptr(),
+            packed_recv_count_2.data_ptr(),
+            expected_m,
+            current_stream,
+            fuse_config->gemm_sms
+        );
+        cudaDeviceSynchronize();
+
+        auto handle = std::make_tuple(packed_recv_src_info, packed_recv_layout_range, num_max_dispatch_tokens_per_rank, hidden_states.size(1), num_experts);
+        torch::Tensor out_2_comm = torch::randn(packed_recv_x.sizes(), dtype(torch::kBFloat16).device(torch::kCUDA));
+        auto [
+            src_info, 
+            layout_range, 
+            num_max_dispatch_tokens_per_rank_, 
+            hidden, 
+            num_experts_
+        ] = handle;
+        cudaDeviceSynchronize();
+
+        get_function_for_gemm(num_tokens, hidden_size, khidden / 2, num_groups, fuse_config->gemm_sms)(
+            std::get<0>(x_fp8_2).data_ptr(), std::get<1>(x_fp8_2).data_ptr(),
+            std::get<0>(y_fp8_2).data_ptr(), std::get<1>(y_fp8_2).data_ptr(),
+            out_2.data_ptr(),
+            packed_recv_count.data_ptr(),
+            expected_m,
+            current_stream,
+            fuse_config->gemm_sms
+        );
+        auto [
+            combine_x,
+            event_,
+            hook_
+        ] = buffer->low_latency_combine(out_2_comm, topk_ids, topk_weights, src_info, layout_range, num_max_dispatch_tokens_per_rank, num_experts, fuse_config->ep_sms, false/*zero_copy*/, false/*async_finish*/, false/*return_recv_hook*/, std::nullopt/*out: inplace tensor*/);
+
+        if (enable_profile) cudaProfilerStop();
+        cudaDeviceSynchronize();
+    }
+
+public:
+    OverlapMoE(uint64_t num_experts, uint64_t num_max_dispatch_tokens_per_rank, uint64_t khidden, uint64_t hidden_size, uint64_t num_tokens, 
+        uint64_t num_topk, uint64_t world_size, std::shared_ptr<ProcessGroupNCCL>& global_pg): 
+        BaseMoE(num_experts, num_max_dispatch_tokens_per_rank, khidden, hidden_size, num_tokens, num_topk, world_size, global_pg) {
+            std::tie(hidden_states, topk_ids, topk_weights, x_fp8, y_fp8, out, o_vec, o_scales, o_scales_strided, silu_out, x_fp8_2, y_fp8_2, out_2) = initialize_random_inputs(num_tokens, num_topk, num_groups, num_experts, m_max, hidden_size, khidden);
+            get_deepep_low_latency_buffer(num_max_dispatch_tokens_per_rank, hidden_size, global_pg, num_groups, buffer,
+                true/*use_cuda_graph*/, true/*use_fp8*/, num_experts, num_tokens);
+    }
+};
