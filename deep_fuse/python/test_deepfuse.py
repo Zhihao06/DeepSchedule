74a4a648766729e6c165ab5d7901b2df7d6e707c
Le Zhihao <130731429+Zhihao06@users.noreply.github.com>
2025-07-18 10:30:15 +0800

    [modify] change test file (#32)

--- deep_fuse/python/test_deepfuse.py
+++ deep_fuse/python/test_deepfuse.py
@@ -34,20 +34,63 @@ def test_deepfuse(rank, world_size):
         global_pg_nccl=dist.group.WORLD._get_backend(torch.device("cuda"))
     )
     w13_weight_fp8, w2_weight_fp8 = get_expert_weights()
-    topk_idx = torch.randint(0, NUM_EXPERTS, (NUM_TOKENS, NUM_TOPK), dtype=torch.int64, device="cuda")
-    topk_weights = torch.randn(NUM_TOKENS, NUM_TOPK, dtype=torch.float32, device="cuda")
-    hidden_states = torch.randn(NUM_TOKENS, HIDDEN_SIZE, dtype=torch.bfloat16, device="cuda")
-
     runtime.create_mode(num_splits=4)
     runtime.load_weights(w13_weight=w13_weight_fp8, w2_weight=w2_weight_fp8)
-    runtime.get_metadata(mode="multi_token", num_tokens=512, num_split_tokens=[128, 128, 128, 128])
-    runtime.load_inputs(mode="multi_token", hidden_states_in=hidden_states, topk_ids_in=topk_idx, topk_weights_in=topk_weights)
-    runtime.launch(mode="multi_token", launch_mode="sched", deepep_sms=20)
-    final_hidden_states = runtime.get_merged_output(mode="multi_token")
+
+    for i in range(50):
+        topk_idx = torch.randint(0, NUM_EXPERTS, (NUM_TOKENS, NUM_TOPK), dtype=torch.int64, device="cuda")
+        topk_weights = torch.randn(NUM_TOKENS, NUM_TOPK, dtype=torch.float32, device="cuda")
+        hidden_states = torch.randn(NUM_TOKENS, HIDDEN_SIZE, dtype=torch.bfloat16, device="cuda")
+        runtime.get_metadata(mode="multi_token", num_tokens=512, num_split_tokens=[256, 256])
+        runtime.load_inputs(mode="multi_token", hidden_states_in=hidden_states, topk_ids_in=topk_idx, topk_weights_in=topk_weights)
+        runtime.launch(mode="multi_token", launch_mode="sched", deepep_sms=20)
+        final_hidden_states = runtime.get_merged_output(mode="multi_token")
 
     torch.distributed.barrier()
     dist.destroy_process_group()
     
+def test_comm_interface(rank, world_size):
+    dist.init_process_group("nccl", init_method="tcp://127.0.0.1:29500", rank=rank, world_size=world_size)
+    backend = dist.group.WORLD._get_backend(torch.device("cuda"))
+    print("backend rank", backend.rank())
+    torch.cuda.set_device(backend.rank())
+    runtime = deep_fuse.Tool(
+        num_experts=NUM_EXPERTS, 
+        num_max_dispatch_tokens_per_rank=NUM_MAX_DISPATCH_TOKENS_PER_RANK, 
+        khidden=KHIDDEN, 
+        hidden_size=HIDDEN_SIZE, 
+        num_tokens=NUM_TOKENS, 
+        num_topk=NUM_TOPK, 
+        world_size=world_size, 
+        global_pg_nccl=dist.group.WORLD._get_backend(torch.device("cuda"))
+    )
+    w13_weight_fp8, w2_weight_fp8 = get_expert_weights()
+    runtime.create_mode(num_splits=4)
+    runtime.load_weights(w13_weight=w13_weight_fp8, w2_weight=w2_weight_fp8)
+
+    for i in range(50):
+        topk_idx = torch.randint(0, NUM_EXPERTS, (NUM_TOKENS, NUM_TOPK), dtype=torch.int64, device="cuda")
+        topk_weights = torch.randn(NUM_TOKENS, NUM_TOPK, dtype=torch.float32, device="cuda")
+        hidden_states = torch.randn(NUM_TOKENS, HIDDEN_SIZE, dtype=torch.bfloat16, device="cuda")
+
+        final_hidden_states = torch.randn(NUM_EXPERTS // world_size, NUM_MAX_DISPATCH_TOKENS_PER_RANK * world_size, HIDDEN_SIZE, dtype=torch.bfloat16, device="cuda")
+        (
+            hidden_states,
+            masked_m,
+            expected_m
+        ) = runtime.low_latency_dispatch_interface(
+            mode="sequence",
+            deepep_sms=20
+        )
+        final_hidden_states = runtime.low_latency_combine_interface(
+            mode="sequence",
+            compute_result=final_hidden_states,
+            deepep_sms=20
+        )
+
+    torch.distributed.barrier()
+    dist.destroy_process_group()
+
 
 if __name__ == "__main__":
     os.environ["NCCL_DEBUG"] = "WARN"
